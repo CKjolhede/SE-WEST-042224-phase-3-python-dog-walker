c84651c8eeda01c0c175f256bca67e1d42d01e89
betalantz <betalantz@gmail.com>
2024-06-05 13:09:36 -0700

    end of lecture 03

--- 03_oop/debug.py
+++ 03_oop/debug.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
+import ipdb
 from lib.pet import *
 
+cookie = Pet("Cookie", 5, "Dachshund", "fiesty", "./assets/cookie.jpg")
+rose = Pet("Rose", 10, "domestic longhair", "sweet", "./assets/rose.jpg")
 
-import ipdb; ipdb.set_trace()
+print(rose)
+rose.print_pet_details()
+
+rose.age = "nothin' but a number"
+print(rose.age)
+
+cookie.age = 6
+print(cookie.age)
+
+print(cookie == rose)
+
+ipdb.set_trace()
